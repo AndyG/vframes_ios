a843e3c3a0ecbb6ac78f6860a76f1f2ceb4daf57
angarron <andy.garron@citrix.com>
2016-01-27 18:01:10 -0800

    added characterID enum

---
+++ VFrames/CharacterID.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterID.swift
+//  VFrames
+//
+//  Created by Andy Garron on 1/27/16.
+//  Copyright © 2016 VFrames. All rights reserved.
+//
+
+import Foundation
+
+enum CharacterID {
+    case RYU,
+    CHUN
+}
