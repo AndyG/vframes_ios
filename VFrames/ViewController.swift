2191adccc6f2f0358f5bec8cac2aee0dbab69810
angarron <andy.garron@citrix.com>
2016-01-27 18:47:49 -0800

    added a text file and read it

--- VFrames/ViewController.swift
+++ VFrames/ViewController.swift
@@ -15,12 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print(CharacterID.CHUN)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func characterButtonClicked(sender: UIButton) {
+        switch (sender.tag) {
+            case 0:
+                print("ryu clicked")
+            case 1:
+                print ("chun clicked")
+        default:
+            print ("invalid clicked")
+        }
+    }
+    
+    @IBAction func loadDataPressed(sender: UIButton) {
+        print("loadDataPressed")
+        loadFile()
+    }
+    
+    func loadFile() {
+        let path = NSBundle.mainBundle().pathForResource("vframes_data", ofType: "json")
+        let jsonData = NSData(contentsOfMappedFile: path!)
+        let dataString = String(data: jsonData!, encoding: NSUTF8StringEncoding)
+        print(dataString!)
+    }
 }
 
